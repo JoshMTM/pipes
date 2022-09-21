e21377367ada6dcfe64206647a5d6336b0712984
Joshua Mitima <joshuamitima@dhcp-9-236-167-58.068-nl.ibmmobiledemo.com>
2022-09-21 15:59:06 +0200

    genenrating new pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,17 @@ export class AppComponent {
   name = '';
   date = '';
   amount!: number;
+  miles!: number;
+
+  car = {
+    make: "Toyota",
+    model: "Camry",
+    year: "2000"
+  };
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
 
   onNameChange( value: string ) {
     this.name = value;
